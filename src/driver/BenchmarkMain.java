be7407230e02f5c5aa20a83d03a1fda271078ad8
Dimitar Misev <dimitarmisev@gmail.com>
2015-01-29 20:08:37 +0000

    update sciql

--- src/driver/BenchmarkMain.java
+++ src/driver/BenchmarkMain.java
@@ -1,6 +1,5 @@
 package driver;
 
-import framework.Benchmark;
 import framework.context.BenchmarkContext;
 import framework.context.ConnectionContext;
 import framework.context.RasdamanContext;
@@ -60,7 +59,7 @@ public class BenchmarkMain {
                 collectionSizes.put(1073741824l, "1Gb");
                 collectionSizes.put(10737418240l, "10Gb");
 
-                for (int noOfDim = 6; noOfDim <= 6; ++noOfDim) {
+                for (int noOfDim = 1; noOfDim <= 6; ++noOfDim) {
 
                     System.out.println("Rasdaman Benchmarking: " + noOfDim + "D");
                     {
@@ -91,8 +90,8 @@ public class BenchmarkMain {
             }
             case "SciDB": {
 
-                SciDBContext scidbContext = new SciDBContext("/conf/scidb.properties");
-                BenchmarkContext benchContext = new BenchmarkContext("/conf/benchmark.properties");
+                SciDBContext scidbContext = new SciDBContext("conf/scidb.properties");
+                BenchmarkContext benchContext = new BenchmarkContext("conf/benchmark.properties");
                 int noQueries = 10;
 
                 Map<Long, String> collectionSizes = new TreeMap<>();
@@ -135,30 +134,33 @@ public class BenchmarkMain {
             case "SciQL": {
 
                 Map<Long, String> collectionSizes = new TreeMap<>();
-                collectionSizes.put(1024l, "1Kb");
-                collectionSizes.put(102400l, "100Kb");
-                collectionSizes.put(1048576l, "1Mb");
+//                collectionSizes.put(1024l, "1Kb");
+//                collectionSizes.put(102400l, "100Kb");
+//                collectionSizes.put(1048576l, "1Mb");
                 collectionSizes.put(104857600l, "100Mb");
                 collectionSizes.put(1073741824l, "1Gb");
 //                collectionSizes.put(10737418240l, "10Gb");
 
-                ConnectionContext sciqlConnection = new ConnectionContext("/conf/sciql.properties");
-                BenchmarkContext benchContext = new BenchmarkContext("/conf/benchmark.properties");
+                ConnectionContext sciqlConnection = new ConnectionContext("conf/sciql.properties");
+                BenchmarkContext benchContext = new BenchmarkContext("conf/benchmark.properties");
                 int noQueries = 10;
 
                 for (int noOfDim = 1; noOfDim <= 6; ++noOfDim) {
                     for (Map.Entry<Long, String> longStringEntry : collectionSizes.entrySet()) {
                         System.out.println("SciQLQueryExecutor: " + noOfDim + "D");
-                        String colName = String.format("colD%dS%s", noOfDim, longStringEntry.getValue());
+                        String colName = String.format("cold%ds%s", noOfDim, longStringEntry.getValue());
                         long collectionSize = longStringEntry.getKey();
                         long maxSelectSize = (long) ((double) collectionSize / 10.0);
+                        if (collectionSize == 104857600l && noOfDim < 4) {
+                            continue;
+                        }
 
                         benchContext.setCollName1(colName);
                         benchContext.setCollSize(collectionSize);
                         benchContext.setMaxQuerySelectSize(maxSelectSize);
 
                         SciQLQueryGenerator queryGenerator = new SciQLQueryGenerator(benchContext, noOfDim, noQueries);
-                        SciQLSystemController systemController = new SciQLSystemController("/conf/system.properties", sciqlConnection);
+                        SciQLSystemController systemController = new SciQLSystemController("conf/system.properties", sciqlConnection);
                         SciQLQueryExecutor queryExecutor = new SciQLQueryExecutor(sciqlConnection, systemController, benchContext, noOfDim);
 
                         systemController.restartSystem();
