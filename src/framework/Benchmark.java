20e3a8cb81b6fe39df45ebc2bbc0742977914538
George Merticariu <merticariug@gmail.com>
2015-01-30 13:36:26 +0000

    added benchmark point query

--- src/framework/Benchmark.java
+++ src/framework/Benchmark.java
@@ -1,11 +1,13 @@
 package framework;
 
 import data.BenchmarkQuery;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
 import util.IO;
 
 /**
@@ -35,46 +37,49 @@ public class Benchmark {
             // the query executor should check whether a collection is already created
 //            queryExecutor.createCollection();
 
-//            List<BenchmarkQuery> benchmarkQueries = queryGenerator.getBenchmarkQueries();
-//
-//            for (BenchmarkQuery query : benchmarkQueries) {
-            BenchmarkQuery query = queryGenerator.getMiddlePointQuery();
-            System.out.printf("Executing query: \"%s\"\n", query.getQueryString());
+            List<BenchmarkQuery> benchmarkQueries = queryGenerator.getBenchmarkQueries();
 
-            List<Long> queryExecutionTimes = new ArrayList<>();
-            for (int repeatIndex = 0; repeatIndex < REPEAT_NO; ++repeatIndex) {
-                boolean failed = true;
-                long time = -1;
+            BenchmarkQuery pointQuery = queryGenerator.getMiddlePointQuery();
+            benchmarkQueries.add(pointQuery);
 
-                for (int retryIndex = 0; retryIndex < MAX_RETRY && failed; ++retryIndex) {
-                    try {
-                        systemController.restartSystem();
-                        time = queryExecutor.executeTimedQuery(query.getQueryString());
-                        failed = false;
-                    } catch (Exception ex) {
-                        System.out.printf("Query \"%s\" failed on try %d. Retrying...\n", query.getQueryString(), retryIndex + 1);
+            for (BenchmarkQuery query : benchmarkQueries) {
+
+                System.out.printf("Executing query: \"%s\"\n", query.getQueryString());
+
+                List<Long> queryExecutionTimes = new ArrayList<>();
+                for (int repeatIndex = 0; repeatIndex < REPEAT_NO; ++repeatIndex) {
+                    boolean failed = true;
+                    long time = -1;
+
+                    for (int retryIndex = 0; retryIndex < MAX_RETRY && failed; ++retryIndex) {
+                        try {
+                            systemController.restartSystem();
+                            time = queryExecutor.executeTimedQuery(query.getQueryString());
+                            failed = false;
+                        } catch (Exception ex) {
+                            System.out.printf("Query \"%s\" failed on try %d. Retrying...\n", query.getQueryString(), retryIndex + 1);
+                        }
                     }
+                    queryExecutionTimes.add(time);
                 }
-                queryExecutionTimes.add(time);
-            }
 
-            StringBuilder resultLine = new StringBuilder();
-            resultLine.append(String.format("\"%s\", \"%s\", \"%s\", \"%d\", \"%d\", \"%d\", ", systemController.getSystemName(), query.getQueryType().toString(), query.getQueryString(), query.getDimensionality(), collectionSize, maxSelectSize));
-            boolean isFirst = true;
+                StringBuilder resultLine = new StringBuilder();
+                resultLine.append(String.format("\"%s\", \"%s\", \"%s\", \"%d\", \"%d\", \"%d\", ", systemController.getSystemName(), query.getQueryType().toString(), query.getQueryString(), query.getDimensionality(), collectionSize, maxSelectSize));
+                boolean isFirst = true;
 
-            for (Long queryExecutionTime : queryExecutionTimes) {
-                if (!isFirst) {
-                    resultLine.append(", ");
+                for (Long queryExecutionTime : queryExecutionTimes) {
+                    if (!isFirst) {
+                        resultLine.append(", ");
+                    }
+                    resultLine.append("\"");
+                    resultLine.append(queryExecutionTime);
+                    resultLine.append("\"");
+                    isFirst = false;
                 }
-                resultLine.append("\"");
-                resultLine.append(queryExecutionTime);
-                resultLine.append("\"");
-                isFirst = false;
-            }
 
-            pr.println(resultLine.toString());
-            pr.flush();
-//            }
+                pr.println(resultLine.toString());
+                pr.flush();
+            }
         } finally {
 //            queryExecutor.dropCollection();
         }
